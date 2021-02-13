bdc52bf4703998a60ef1a952723c89ae9a1de7cf
netbyte <alex.tsai@protonmail.com>
2021-02-13 05:01:26 +0000

    support arm64

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,11 @@
 #!bin/bash
 UNAME=$(uname)
+ARCH=$(uname -m)
 
-if [ "$UNAME" == "Linux" ] ; then
+if [[ "$UNAME" == "Linux" && "$ARCH" == "x86_64" ]] ; then
     GOOS=linux GOARCH=amd64 go build -o ./bin/tcptun ./main.go
+elif [[ "$UNAME" == "Linux" && "$ARCH" == "aarch64" ]] ; then
+    GOOS=linux GOARCH=arm64 go build -o ./bin/tcptun_arm64 ./main.go
 elif [ "$UNAME" == "Darwin" ] ; then
     GOOS=darwin GOARCH=amd64 go build -o ./bin/tcptun ./main.go
 elif [[ "$UNAME" == CYGWIN* || "$UNAME" == MINGW* ]] ; then
