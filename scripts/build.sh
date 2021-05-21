00bba0498dda9bc72e4c8303c7d5156af0e52633
alex tsai <alex.tsai@protonmail.com>
2021-05-21 22:32:57 +0800

    update script

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,6 +6,7 @@ ARCH=$(uname -m)
 
 if [[ "$UNAME" == "Linux" && "$ARCH" == "x86_64" ]] ; then
     GOOS=linux GOARCH=amd64 go build -o ./bin/tcptun ./main.go
+    CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -ldflags="-s -w" -o ./bin/tcptun-openwrt-amd64 ./main.go
 elif [[ "$UNAME" == "Linux" && "$ARCH" == "aarch64" ]] ; then
     GOOS=linux GOARCH=arm64 go build -o ./bin/tcptun_arm64 ./main.go
 elif [ "$UNAME" == "Darwin" ] ; then
