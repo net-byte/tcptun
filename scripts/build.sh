9289c2b8e5adbc689b855c171b3cf339f805bc17
Alex Tsai <alex.tsai@pdabc.com>
2021-05-25 15:55:39 +0800

    update script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,18 +1,13 @@
 #!bin/bash
 
 export GO111MODULE=on
-UNAME=$(uname)
-ARCH=$(uname -m)
-
-if [[ "$UNAME" == "Linux" && "$ARCH" == "x86_64" ]] ; then
-    GOOS=linux GOARCH=amd64 go build -o ./bin/tcptun ./main.go
-    CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -ldflags="-s -w" -o ./bin/tcptun-openwrt-amd64 ./main.go
-elif [[ "$UNAME" == "Linux" && "$ARCH" == "aarch64" ]] ; then
-    GOOS=linux GOARCH=arm64 go build -o ./bin/tcptun_arm64 ./main.go
-elif [ "$UNAME" == "Darwin" ] ; then
-    GOOS=darwin GOARCH=amd64 go build -o ./bin/tcptun ./main.go
-elif [[ "$UNAME" == CYGWIN* || "$UNAME" == MINGW* ]] ; then
-    GOOS=windows GOARCH=amd64 go build -o ./bin/tcptun.exe ./main.go
-fi
+#Linux
+GOOS=linux GOARCH=amd64 go build -o ./bin/tcptun-linux-amd64 ./main.go
+#Mac OS
+GOOS=darwin GOARCH=amd64 go build -o ./bin/tcptun-darwin-amd64 ./main.go
+#Windows
+GOOS=windows GOARCH=amd64 go build -o ./bin/tcptun-windows-amd64.exe ./main.go
+#Openwrt
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -ldflags="-s -w" -o ./bin/tcptun-openwrt-amd64 ./main.go
 
 echo "done!"
