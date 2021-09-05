58d877c1c6212d99c50e523da59c4b1dbd59d57c
alex <alex@ubuntu.lan>
2021-09-05 13:18:45 +0800

    update script

--- scripts/build.sh
+++ scripts/build.sh
@@ -12,4 +12,4 @@ GOOS=windows GOARCH=amd64 go build -o ./bin/tcptun-windows-amd64.exe ./main.go
 #Openwrt
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -ldflags="-s -w" -o ./bin/tcptun-openwrt-amd64 ./main.go
 
-echo "done!"
+echo "DONE!!!"
